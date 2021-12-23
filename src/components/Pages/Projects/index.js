b9bffa6a277447e63ac42933fa105acbdc9c8c75
Juan Jose Ramirez <jjramirez@miuandes.cl>
2021-12-23 12:22:50 -0800

    triptinerary added

--- src/components/Pages/Projects/index.js
+++ src/components/Pages/Projects/index.js
@@ -4,23 +4,31 @@ import Container from 'react-bootstrap/Container'
 import './style.css';
 import bird from './images/bird.jpeg'
 import hero from './images/hero.png'
+import triptinerary from './images/background-compressed2.jpg'
 
 function Projects() {
     const projects = [
+        {
+            image: triptinerary,
+            title: 'Triptinerary',
+            deployedUrl: 'https://triptinerary.herokuapp.com/',
+            repoUrl: 'https://github.com/Triptinerary/travel-app',
+            description: 'Place for “travelpreneurs” to exclusively share their travel experiences, monetize them, and build their brand. In this project, I build the back-end, using MongoDB and Express.js, to handle the controllers.'
+        },
         {
             image: bird,
             title: 'Birds Eye View',
             deployedUrl: 'https://abirdseyeview.herokuapp.com/',
             repoUrl: 'https://github.com/JuanjoRamirez262/birdsblog',
-            // description: 'A web application design to post birds sightings. Created using Node.js, Express.js, MySQL and Bootstraps'
+            description: 'A web application design to post birds sightings. Created using Node.js, Express.js, MySQL and Bootstraps'
         },
         {
             image: hero,
             title: 'Regional Holiday Calendar',
             deployedUrl: 'https://lemelisk27.github.io/country-holiday/',
             repoUrl: 'https://github.com/Lemelisk27/country-holiday',
-            // description: 'This code was created to know the situation in which the different countries are regarding the cases of covid-19. The national holidays of each country are also shown, so with this information, to be able to make a wise decision when traveling to a country.'
-        }
+            description: 'This code was created to know the situation in which the different countries are regarding the cases of covid-19. The national holidays of each country are also shown, so with this information, to be able to make a wise decision when traveling to a country.'
+        },
         // {
         //     image: '#',
         //     title: '',
@@ -37,18 +45,21 @@ function Projects() {
         // }
     ]
     return (
-        <Container className="card-container justify-content-center">
-            {projects.map((project, index) => (
+        <Container>
+            <Container className="card-container justify-content-center">
+                {projects.map((project, index) => (
                     <Card bg="light" key={index} text="dark" className="card">
                         <Card.Img className="project-image" variant="top" src={project.image} />
                         <Card.Body className="card-body">
-                        <Card.Title>{project.title}</Card.Title>
+                            <Card.Title>{project.title}</Card.Title>
+                            <Card.Text>{project.description}</Card.Text>
                             <Card.Link href={project.deployedUrl}>Deployed app link</Card.Link>
                             <Card.Link href={project.repoUrl}>Repo link</Card.Link>
                         </Card.Body>
                     </Card>
                 )
-            )}
+                )}
+            </Container>
         </Container>
     )
 }
